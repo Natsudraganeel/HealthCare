175109ab9e6e77ff844fd8a11b28665ea650a994
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2024-07-25 20:16:49 +0530

    Update Home.jsx

--- my-Health-Innovation/src/components/Home.jsx
+++ my-Health-Innovation/src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import homeBg from '../assets/img/home_bg2.jpg'; 
 
 const Home=()=>{
+     const backgroundImage = `url(${homeBg})`;
     return (
         
-        <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 text-black  bg-gradient-to-b from-green-200 to-green-300 bg-no-repeat bg-cover" style={{ backgroundImage: `url('../src/assets/img/home_bg2.jpg')` }}>
+        <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 text-black  bg-gradient-to-b from-green-200 to-green-300 bg-no-repeat bg-cover" style={{ backgroundImage: backgroundImage }}>
             <div className="w-full  lg:w-4/5 space-y-5 mt-10 ">
        
                  <h1 className="text-5xl font-bold leading-tight ">Empowering Health Choices for a Vibrant Life Your Trusted..</h1>
