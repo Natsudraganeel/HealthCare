9d31a5462c2d20b0c4513826f63ea84b707412b1
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2024-07-25 20:18:50 +0530

    Update Nutrient.jsx

--- my-Health-Innovation/src/components/Nutrient.jsx
+++ my-Health-Innovation/src/components/Nutrient.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import NutrientCard from "../layouts/NutrientCard"
 import {RiMicroscopeLine} from "react-icons/ri"
 import {MdHealthAndSafety} from "react-icons/md"
-//import {FaHeartBeat} from "react-icons/fa"
+import {FaHeartbeat} from "react-icons/fa"
 
 
 const Nutrient=()=>
 {
     const icon1=<RiMicroscopeLine size={35} className="text-backgroundColor"/>
     const icon2=<MdHealthAndSafety size={35} className="text-backgroundColor"/>
-    //const icon3=<FaHeartBeat size={35} className="text-backgroundColor"/>
+    const icon3=<FaHeartbeat size={35} className="text-backgroundColor"/>
     return (
         <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 pt-24 lg:pt-16">
             <div className="flex flex-col items-center lg:flex-row justify-between">
@@ -24,7 +24,7 @@ const Nutrient=()=>
             <div className="flex flex-col lg:flex-row gap-5 pt-14">
                 <NutrientCard icon={icon1} title="Lab Test"/>
                 <NutrientCard icon={icon2} title="Lab Test2"/>
-                {/*<NutrientCard icon={icon3} title="Lab Test3"/>*/}
+                <NutrientCard icon={icon3} title="Lab Test3"/>
                 
             </div>
         </div>
