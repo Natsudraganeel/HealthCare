d8889911afecf8c75da08f858e4dd97ebefa1ef6
Ajay-sen66 <128738780+Ajay-sen@users.noreply.github.com>
2024-07-26 18:27:34 +0530

    Update Home.jsx

--- my-Health-Innovation/src/components/Home.jsx
+++ my-Health-Innovation/src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home=()=>{
             <div className="w-full  lg:w-4/5 space-y-5 mt-10 ">
        
                  <h1 className="text-5xl font-bold leading-tight ">Empowering Health Choices for a Vibrant Life Your Trusted..</h1>
-                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni vel minima aut accusamus, doloribus quaerat. Laborum voluptatum mollitia sed ut iste earum! Ducimus, quia quibusdam esse illo magni earum saepe?</p>
+                 <p>Driven by a mission to provide accessible, high-quality, and patient-centered care to all individuals, leveraging advanced technology and compassionate service. Our vision is to revolutionize healthcare delivery by creating an integrated ecosystem that promotes holistic well-being, fosters innovation, and ensures equitable access to medical resources, ultimately transforming the healthcare landscape for a healthier, more connected world.</p>
 
                  <a href="https://en.wikipedia.org/wiki/Health_care" target="_blank" rel="noopener noreferrer">
   <button className='bg-brightColor text-white px-4 py-2 rounded-md hover:bg-hoverColor transition duration-300 ease-in-out'>
