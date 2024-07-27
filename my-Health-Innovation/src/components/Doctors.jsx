51c4ecda1f6984ecc24065da1ee83ba1a005be55
Ajay-sen66 <128738780+Ajay-sen@users.noreply.github.com>
2024-07-27 19:34:46 +0530

    Update Doctors.jsx

--- my-Health-Innovation/src/components/Doctors.jsx
+++ my-Health-Innovation/src/components/Doctors.jsx
@@ -113,7 +113,7 @@ setDoctors(res.data.doctors);
         <button  onClick={()=>{window.location.reload()}} className=" doctor my-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">All</button>
       </div>
       <h1 style={head}>Consult Top Doctors By Speciality For Any Health Concern </h1>
-      <div className="allcard">
+      <div className="allcard ml-4">
         {doctors.map((doctor) => {
           return <Card
             key={doctor._id}
