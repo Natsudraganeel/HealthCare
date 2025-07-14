306eb32d91f824c525a7b88222072141330a24e3
Ajay-sen66 <128738780+Ajay-sen@users.noreply.github.com>
2025-07-14 17:11:20 +0530

    Update DoctorForm.jsx

--- my-Health-Innovation/src/components/DoctorForm.jsx
+++ my-Health-Innovation/src/components/DoctorForm.jsx
@@ -154,9 +154,22 @@ export default function DoctorForm() {
                         <h2 className="py-2 block" style={h2}>Personal information</h2>
                         <div style={inputGroup}>
                             <div style={inputContainer} className="input-container">
-                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
-                                <input onChange={onChange} style={input} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" name="name" id="name" required />
-                            </div>
+                              <label htmlFor="name" className="block mb-2 text-sm font-medium text-black dark:text-white">
+                                Full Name
+                              </label>
+                              <input 
+                                onChange={onChange} 
+                                style={input} 
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
+                                   focus:ring-blue-500 focus:border-blue-500 block p-2.5 
+                                   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+                                   dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                type="text" 
+                                name="name" 
+                                id="name" 
+                                required 
+                              />
+                        </div>
                         </div>
                     </section>
                     <section style={section}>
