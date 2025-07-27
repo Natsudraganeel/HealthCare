c2b792264d11ffaa450865613311e6a407a4fad6
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2025-07-27 18:28:46 +0530

    Update index.js

--- srijan_24_hospital_backend/index.js
+++ srijan_24_hospital_backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
 const connectToMongoDB = require('./db');
 
 const corsOptions = {
@@ -14,23 +14,19 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 const PORT = process.env.PORT || 8000;
-// Serve static files (Vite build)
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
 // ** Routes ** //
 app.use("/api/auth", require('./routes/auth.js'))
 app.use("/api/doctors", require('./routes/doctors.js'))
 app.use("/api/appointment", require('./routes/appointment.js'))
 app.use("/api/patients", require('./routes/patient.js'))
 app.use("/api/medicalrecords", require('./routes/medicalRecord.js'))
-//     app.get("/",async(req,res)=>{
-//     res.send("Hello World");
-// })
+    app.get("/",async(req,res)=>{
+    res.send("Hello World");
+})
 
 
-//  Fallback to index.html for client-side routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+
 ; (async () => {
     await connectToMongoDB(process.env.MONGODB_URL);
 
