4506955ecdbb3530a1637eb31c3fb68f4c118a5b
Natsudraganeel <149077586+Natsudraganeel@users.noreply.github.com>
2025-07-27 17:12:05 +0530

    Update doctordashboard.jsx

--- my-Health-Innovation/src/components/doctordashboard.jsx
+++ my-Health-Innovation/src/components/doctordashboard.jsx
@@ -37,7 +37,7 @@ export default function DoctorDashboard() {
   }
 
   const getDoctor = async () => {
-    const response = await fetch(`http://localhost:8000/api/doctors/getdoctorbyuserid`, {
+    const response = await fetch(`https://healthcare-backend-z0xu.onrender.com/api/doctors/getdoctorbyuserid`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -65,7 +65,7 @@ export default function DoctorDashboard() {
   const handleSubmit = async (event) => {
     event.preventDefault()
     const { doctorName, patientName, date, medicines, advice } = credentials;
-    const response = await fetch(`http://localhost:8000/api/medicalrecords/createmedicalrecord/${patientId}`, {
+    const response = await fetch(`https://healthcare-backend-z0xu.onrender.com/api/medicalrecords/createmedicalrecord/${patientId}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -86,7 +86,7 @@ export default function DoctorDashboard() {
         const index = ans.findIndex((item => item.id === id));
         ans.splice(index, 1);
         setAppointments(ans);
-        const response = await fetch(`http://localhost:8000/api/appointment/delete-appintment/${id}`, {
+        const response = await fetch(`https://healthcare-backend-z0xu.onrender.com/api/appointment/delete-appintment/${id}`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
